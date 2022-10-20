a223dcfa596d10f797fa09821dafb50a15b7523b
kim jeongg hoon <48915641+chkpizza@users.noreply.github.com>
2022-10-20 12:36:56 +0900

    MainActivity 하단 탐색바 구조 변경, daily_record_nav_graph 추가

--- app/src/main/java/com/sesac/planet/presentation/view/main/daily_record/DailyRecordFragment.kt
+++ app/src/main/java/com/sesac/planet/presentation/view/main/daily_record/DailyRecordFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.sesac.planet.databinding.FragmentDailyRecordBinding
 
 class DailyRecordFragment : Fragment() {
@@ -19,6 +20,22 @@ class DailyRecordFragment : Fragment() {
         _binding = FragmentDailyRecordBinding.inflate(inflater, container, false)
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initialize()
+    }
+
+    private fun initialize() {
+        binding.pastRecordButton.setOnClickListener {
+            findNavController().navigate(DailyRecordFragmentDirections.actionDailyRecordFragmentToPastRecordActivity())
+        }
+        binding.writeDailyRecordButton.setOnClickListener {
+            findNavController().navigate(DailyRecordFragmentDirections.actionDailyRecordFragmentToWriteDailyRecordActivity())
+        }
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
---
+++ app/src/main/java/com/sesac/planet/presentation/view/main/daily_record/PastRecordActivity.kt
@@ -0,0 +1,12 @@
+package com.sesac.planet.presentation.view.main.daily_record
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sesac.planet.R
+
+class PastRecordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_past_record)
+    }
+}
