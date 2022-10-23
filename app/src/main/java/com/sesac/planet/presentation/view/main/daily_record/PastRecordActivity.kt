f90fd25b366a7f00fbf35b3000f805abc7870b2e
kim jeongg hoon <48915641+chkpizza@users.noreply.github.com>
2022-10-23 12:03:26 +0900

    PastRecordActivity Tab-Viewpager2 연결

--- app/src/main/java/com/sesac/planet/presentation/view/main/daily_record/PastRecordActivity.kt
+++ app/src/main/java/com/sesac/planet/presentation/view/main/daily_record/PastRecordActivity.kt
@@ -2,12 +2,15 @@ package com.sesac.planet.presentation.view.main.daily_record
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.tabs.TabLayoutMediator
 import com.sesac.planet.R
 import com.sesac.planet.databinding.ActivityPastRecordBinding
+import com.sesac.planet.presentation.view.main.daily_record.adapter.PastRecordAdapter
 import com.sesac.planet.utility.SystemUtility
 
 class PastRecordActivity : AppCompatActivity() {
     private val binding by lazy { ActivityPastRecordBinding.inflate(layoutInflater)}
+    private val pastRecordAdapter by lazy { PastRecordAdapter(supportFragmentManager, lifecycle)}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,5 +21,16 @@ class PastRecordActivity : AppCompatActivity() {
 
     private fun initialize() {
         SystemUtility.makeFullScreen(window, binding.root)
+        SystemUtility.applyWindowInsetsPadding(binding.root)
+
+        initTabs()
+    }
+
+    private fun initTabs() {
+        binding.fragmentContainerView.adapter = pastRecordAdapter
+
+        TabLayoutMediator(binding.tabs, binding.fragmentContainerView) { tab, position ->
+            tab.text = pastRecordAdapter.getCurrentTabTitle(position)
+        }.attach()
     }
 }
