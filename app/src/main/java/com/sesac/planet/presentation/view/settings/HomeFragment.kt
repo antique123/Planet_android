b3aa39918f2b766b012c9163aa680247dd8a43eb
rnfejrajrdj <89388408+rnfejrajrdj@users.noreply.github.com>
2022-10-25 17:16:29 +0900

    (New) 홈 화면: 할 일 추가 구현

--- app/src/main/java/com/sesac/planet/presentation/view/settings/HomeFragment.kt
+++ app/src/main/java/com/sesac/planet/presentation/view/settings/HomeFragment.kt
@@ -7,10 +7,11 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.sesac.planet.databinding.DialogHomeAddToDoBinding
 import com.sesac.planet.databinding.FragmentHomeBinding
 import com.sesac.planet.presentation.view.settings.adapter.HomeTodayGrowthPlanAdapter
 
-class HomeFragment : Fragment()  {
+class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
     private lateinit var homeTodayGrowthPlanAdapter: HomeTodayGrowthPlanAdapter
@@ -28,15 +29,23 @@ class HomeFragment : Fragment()  {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        initViews()
+    }
+
+    private fun initViews() {
         initHomeTodayGrowthRecyclerView(isShowMore)
 
         binding.homeShowMoreBtn.setOnClickListener {
             isShowMore = !isShowMore
             initHomeTodayGrowthRecyclerView(isShowMore)
         }
+
+        binding.homeAddToDoBtn.setOnClickListener {
+            HomeAddToDoDialog(requireContext()).show()
+        }
     }
 
-    private fun initHomeTodayGrowthRecyclerView(isShowMore:Boolean){
+    private fun initHomeTodayGrowthRecyclerView(isShowMore: Boolean) {
         val items = mutableListOf<String>().apply {
             add("포폴만들기")
             add("채용정보 확인하기")
@@ -50,7 +59,8 @@ class HomeFragment : Fragment()  {
         }
 
         homeTodayGrowthPlanAdapter = HomeTodayGrowthPlanAdapter(items, isShowMore)
-        binding.homeAddToDoRecyclerView.layoutManager = LinearLayoutManager(requireContext(), RecyclerView.VERTICAL, false)
+        binding.homeAddToDoRecyclerView.layoutManager =
+            LinearLayoutManager(requireContext(), RecyclerView.VERTICAL, false)
         binding.homeAddToDoRecyclerView.adapter = homeTodayGrowthPlanAdapter
     }
 
